b711fe375370444ad3a274260dbbf5d4f1312a69
rohitraj4169 <rohitraj@startbitsolutions.com>
2023-04-25 11:52:54 +0530

    admin user created by seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email: 'rohit@example.com', first_name: 'Rohit',first_name:'Raj', password: 'admin123', password_confirmation: 'admin123', role_id:1)
